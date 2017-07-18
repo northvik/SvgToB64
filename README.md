76e0dcf8fa4746df1be1bd19c5de992b0946577e
Camille <camille@versantus.co.uk>
2017-07-18 14:48:40 +0100

    Script only

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# SvgToB64
-This contain a script but also a web interface that help to generate sass files containing compress and base 64 encode svg images
+# SVG to B64
+
+This script will compress and convert in base 64 all the svg images who are in 
+the select folder, it's going to use the file ```template.scss.txt``` to create sass class
+in the ```_image.scss``` file. You can edit the file ```template.scss.txt``` to be of your liking but 
+be sure to use this 2 variable in it ```@@_IMAGE_NAME_@@``` who use the name of your svg file, and 
+```@@_IMAGE_DATA_@@``` who will be replace by the base 64 string.
+
+```@@_IMAGE_NAME_@@``` who use the name of your svg file.
+```@@_IMAGE_DATA_@@``` who will be replace by the base 64 string.
+
+You can use the converter like this:
+
+```bash
+php convert.php
+```
+This will use the images in the toConvert Folder.
+
+or you can use it like this:
+```bash
+php convert.php ../Path/to/specific/folder
+```
+The path can be relative or absolute
+
+I hope this script will help you.
---
+++ convert.php
@@ -0,0 +1,102 @@
+<?php
+require_once('SvgSanitizer.php');
+
+function compress($svg)
+{
+  $svg = preg_replace('/<!--.*-->/', '', $svg);
+  $svg = preg_replace('/<g>[\n\r\s]*<\/g>/', '', $svg);
+  $svg = preg_replace('/\n/', ' ', $svg);
+  $svg = preg_replace('/\t/', ' ', $svg);
+  $svg = preg_replace('/\s\s+/', ' ', $svg);
+  $svg = str_replace('> <', '><', $svg);
+  $svg = str_replace(';"', '"', $svg);
+
+  return $svg;
+}
+
+function colorize($string){
+  return "\033[1;35m". $string . "\033[0m";
+}
+function error($string){
+  return "\033[1;31m". $string . "\033[0m\n";
+}
+
+echo "
+\033[1;35m################################################################################
+
+                        WELCOME TO THE SVG CONVERTER\033[0m
+
+      This script will compress and convert in base 64 all the svg images who are in 
+      the select folder, it's going to use the file template.scss.txt to create sass class
+      in the _image.scss file. You can edit the file template.scss.txt to be of your liking but 
+      be sure to use this 2 variable in it @@_IMAGE_NAME_@@ who use the name of your file, and 
+      @@_IMAGE_DATA_@@ who will be replace by the base 64 string.
+      
+      \e[1;35m@@_IMAGE_NAME_@@\e[0m who use the name of your file.
+      \e[1;35m@@_IMAGE_DATA_@@\e[0m who will be replace by the base 64 string.
+
+################################################################################\033[0m\n\n
+";
+
+$dir =  "./toConvert";
+
+if (isset($argv[1])){
+  $dir =  $argv[1];
+}else{
+  echo "You didn't specify a path the script is going to use the svg images in ".$dir."\n
+You can specify the path like this:\n".colorize("   php convert.php ../../Downloads/")."
+\nDo you want to continue [Y/n] ";
+  $handle = fopen ("php://stdin","r");
+  $line = fgets($handle);
+  $reply=trim($line);
+  if(!empty($reply) && $reply!='y' && $reply!='Y'){
+    echo error("ABORDING!\n");
+    exit;
+  }
+  fclose($handle);
+}
+echo "Folder used: ".colorize($dir)."\n";
+// only min-width of cells is set
+$mask = "|%-30.30s |%-10.10s  |%-10.10s |%-10.10s \n";
+printf($mask, 'File name ', 'size before', 'size after', '% gain');
+
+// Create a new sanitizer instance
+$sanitizer = new SvgSanitizer();
+
+$template = file_get_contents("template.scss.txt");
+
+$arrayOfFiles = scandir($dir);
+
+$return = "";
+foreach ($arrayOfFiles as $filePath) {
+  $fileInfo = pathinfo($dir."/" . $filePath);
+  if (isset($fileInfo["extension"]) && strtolower($fileInfo["extension"]) == "svg") {
+    $sizeBefore = filesize($dir."/" . $filePath);
+    $sanitizer->load($dir."/" . $filePath );
+
+    // sanitize!
+    $sanitizer->sanitize();
+    file_put_contents("./convert/" . $filePath, compress($sanitizer->saveSVG()));
+    // Print out sanitized SVG
+    $sizeafter = filesize("./convert/" . $filePath);
+
+    $pourcent = (($sizeBefore - $sizeafter) / $sizeBefore) * 100;
+    printf($mask, $filePath, $sizeBefore, $sizeafter, round($pourcent)." %");
+
+
+    $data = "data:image/svg+xml;base64,".base64_encode(file_get_contents("./convert/" . $filePath));
+
+
+    $fileName  = str_replace(" ", "_",$fileInfo["filename"]);
+
+    $templateHidrate = $template;
+    $templateHidrate = str_replace("@@_IMAGE_NAME_@@", $fileName, $templateHidrate);
+    $templateHidrate = str_replace("@@_IMAGE_DATA_@@", $data, $templateHidrate);
+    $return .= $templateHidrate."\n\n";
+    unlink("./convert/" . $filePath);
+  }
+}
+file_put_contents("_images.scss", $return);
+
+echo colorize("Finished\n");
+exit;
